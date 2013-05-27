0cf4507b27866255733d4568fa7e2147cdd2854c
tt <tt@tt-PC>
2013-05-27 17:35:37 +0800

    reset

--- org/chw/proto/ExprLexer.java
+++ org/chw/proto/ExprLexer.java
@@ -21,8 +21,6 @@ public class ExprLexer extends Lexer {
     public static final int Default=11;
     public static final int DefaultValue=12;
     public static final int Dot=13;
-    
-    
     public static final int Enum=14;
     public static final int EnumList=15;
     public static final int Equals=16;
